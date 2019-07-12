e078867aa72f7980c67e728ff9ecb645004dc479
Camila Buenaventura Marquez <camila.buenamar@gmail.com>
2019-07-12 13:52:48 +0100

    woods moving

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const rightWoods = document.querySelectorAll('.right-woods')
   const leftWoods = document.querySelectorAll('.left-woods')
 
+
   let currentIndex = 76
   const width = 9
 
@@ -33,10 +34,68 @@ document.addEventListener('keyup', moveFrog)
     squares[currentIndex].classList.add('frog')
   }
 
+  function moveRightWoods(rightWood) {
+    switch (true) {
+      case rightWood.classList.contains('wm-one'):
+        rightWood.classList.remove('safecross')
+        rightWood.classList.remove('wm-one')
+        rightWood.classList.add('wm-five')
+        break
+      case rightWood.classList.contains('wm-two'):
+        rightWood.classList.remove('wm-two')
+        rightWood.classList.add('wm-one')
+        break
+      case rightWood.classList.contains('wm-three'):
+        rightWood.classList.remove('wm-three')
+        rightWood.classList.add('wm-two')
+        break
+      case rightWood.classList.contains('wm-four'):
+        rightWood.classList.add('safecross')
+        rightWood.classList.remove('wm-four')
+        rightWood.classList.add('wm-three')
+        break
+      case rightWood.classList.contains('wm-five'):
+        rightWood.classList.remove('wm-five')
+        rightWood.classList.add('wm-four')
+        break
+    }
+  }
+
+  function moveLeftWoods(leftWood) {
+      switch (true) {
+        case leftWood.classList.contains('wm-one'):
+          leftWood.classList.remove('wm-one')
+          leftWood.classList.add('wm-two')
+          break
+        case leftWood.classList.contains('wm-two'):
+          leftWood.classList.remove('wm-two')
+          leftWood.classList.add('wm-three')
+          break
+        case leftWood.classList.contains('wm-three'):
+          leftWood.classList.remove('safecross')
+          leftWood.classList.remove('wm-three')
+          leftWood.classList.add('wm-four')
+          break
+        case leftWood.classList.contains('wm-four'):
+          leftWood.classList.remove('wm-four')
+          leftWood.classList.add('wm-five')
+          break
+        case leftWood.classList.contains('wm-five'):
+          leftWood.classList.add('safecross')
+          leftWood.classList.remove('wm-five')
+          leftWood.classList.add('wm-one')
+          break
+      }
+
+  }
+
   // --- Interval
   const timerId = setInterval(() => {
     rightFrogs.forEach(rightFrog => rightFrog.classList.toggle('safecross'))
     leftFrogs.forEach(leftFrog => leftFrog.classList.toggle('safecross'))
+    leftWoods.forEach(leftWood => moveLeftWoods(leftWood))
+    rightWoods.forEach(rightWood => moveRightWoods(rightWood))
+
   }, 1000)
 
   setTimeout(() => {
